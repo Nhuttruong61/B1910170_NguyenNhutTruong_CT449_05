10fc921e88212b6590d237c4d680214221c6f309
Nhuttruong61 <nhuttruong2261@gmail.com>
2023-02-17 20:05:20 +0700

    Cài đặt mongodb, Định nghĩa hàm trợ giúp kết nối và truy xuất dữ liệu

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const app =require('./app');
 const config = require('./app/config');
+const MongoDB = require('./app/utils/mongodb.util');
 
-// start server
-const PORT = config.app.port;
-app.listen(PORT, ()=>{
-    console.log(`server is running on port ${PORT}`)
-});
+
+async function startServer(){
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to database!");
+
+        const PORT = config.app.port;
+        app.listen(PORT, ()=>{
+            console.log(`server is running on port ${PORT}`)
+        });
+        
+    } catch (error) {
+        console.log(" Can't connect to the database!", error);
+        process.exit();
+    }
+
+}
+startServer();
